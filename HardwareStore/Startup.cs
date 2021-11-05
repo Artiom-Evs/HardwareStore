7b5b7238231e0eaf519f07fb87b64f302f65736f
Artiom-Evs <evs.a.s@mail.ru>
2021-11-05 16:36:46 +0300

    Add MVC infrastructure with default folders structure

--- HardwareStore/Startup.cs
+++ HardwareStore/Startup.cs
@@ -16,6 +16,7 @@ namespace HardwareStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +27,13 @@ namespace HardwareStore
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
             app.UseRouting();
-
+            
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
