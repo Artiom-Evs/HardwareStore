c25beec6fe194e8b50aaf114c6bf198ed13b4650
Artiom-Evs <evs.a.s@mail.ru>
2021-11-08 03:13:21 +0300

    Add SeedData class
    THis class adds initial items.

---
+++ HardwareStore/Models/SeedData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HardwareStore.Models
+{
+    public static class SeedData
+    {
+        public static void EnsurePopulated(IApplicationBuilder app)
+        {
+            StoreDbContext context = app.ApplicationServices.CreateScope().ServiceProvider.GetRequiredService<StoreDbContext>();
+
+            if (context.Products.Any())
+            {
+                context.Products.AddRange(
+                    new Product { ProductID = 1, Name = "Product", Price = 100M, Category = "category1", Description = "SomeProduct1" }
+                    );
+            }
+            ;
+        }
+    }
+}
--- HardwareStore/Startup.cs
+++ HardwareStore/Startup.cs
@@ -48,6 +48,8 @@ namespace HardwareStore
             {
                 endpoints.MapDefaultControllerRoute();
             });
+
+            SeedData.EnsurePopulated(app);
         }
     }
 }
