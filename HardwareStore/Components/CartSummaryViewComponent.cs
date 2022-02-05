d0a736e939146d7417adccdd55d0e4b700f87549
Artiom-Evs <evs.a.s@mail.ru>
2022-02-05 20:58:11 +0300

    Add CartSummaryViewComponent
    font-awesome client-side library has been installed.

---
+++ HardwareStore/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using HardwareStore.Models;
+
+namespace HardwareStore.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private Cart cart;
+        public CartSummaryViewComponent(Cart cartService)
+        {
+            this.cart = cartService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(cart);
+        }
+    }
+}
